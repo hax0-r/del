6a00fe92c181f47427b362c2c0d3a61aeab54539
Muhammad <talha18513@gmail.com>
2025-01-01 11:43:12 +0500

    enlist remove

---
+++ src/Component/Nav.jsx
@@ -0,0 +1,9 @@
+import React from 'react'
+
+const Nav = () => {
+  return (
+    <h1 className='text-center font-bold text-5xl min-h-screen flex items-center justify-center'>Nav</h1>
+  )
+}
+
+export default Nav
