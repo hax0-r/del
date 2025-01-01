1cd4f6443b6a9a38ec722ca5b7d1d3f25757c4df
Muhammad <talha18513@gmail.com>
2025-01-01 12:30:14 +0500

    comp

---
+++ src/app/form/page.js
@@ -0,0 +1,60 @@
+"use client"
+import Image from "next/image"
+import school1 from "../../../public/Assets/Home/school1.jpg"
+import logo from '../../../public/Assets/logo.jpg'
+import Link from "next/link"
+import Label from "../Components/Label"
+import Input from "../Components/Input"
+
+const Form = () => {
+    const handleSubmit = async (event) => {
+        event.preventDefault();
+        const formData = new FormData(event.target);
+
+        formData.append("access_key", "YOUR_ACCESS_KEY_IS_WRITTEN_HERE");
+
+        const object = Object.fromEntries(formData);
+        const json = JSON.stringify(object);
+
+        const response = await fetch("https://api.web3forms.com/submit", {
+            method: "POST",
+            headers: {
+                "Content-Type": "application/json",
+                Accept: "application/json"
+            },
+            body: json
+        });
+        const result = await response.json();
+        if (result.success) {
+            console.log(result);
+        }
+    }
+
+    return (
+        <div
+            style={{ background: `url(${school1.src}) center/cover fixed` }}
+            className="bgContactSection  w-full min-h-screen bg-no-repeat flex p-2 items-center justify-center">
+
+            <form className="mx-auto max-w-2xl w-full p-5 glass rounded-2xl">
+                <Link href="/">
+                    <Image src={logo} alt="logo" className='w-20 mx-auto' />
+                </Link>
+                <h2 className="text-white text-center mt-6 font-semibold md:text-2xl text-xl">Download Form</h2>
+
+                <Label name="name" />
+                <Input type="text" name="name" />
+
+                <Label name="email" />
+                <Input type="email" name="email" />
+
+                <Label name="phone" />
+                <Input type="number" name="phone" />
+
+                <button className="hover:bg-[#0056b3] w-full mt-6 font-medium px-10 py-4 cursor-pointer transition-all duration-500 bg-[#007bff] text-white   rounded-lg ">Download and Submit</button>
+            </form>
+
+        </div>
+    )
+}
+
+export default Form
--- src/app/page.js
+++ src/app/page.js
@@ -1,9 +1,16 @@
-import { CARD_DATA } from "@/Data/Data";
+import { CARD_DATA, wyChooseUs } from "@/Data/Data";
 import Header from "./Components/Header";
 import Navbar from "./Components/Navbar";
 import ScrollToTop from "./Components/ScrollToTop";
 import Slider from "./Components/Slider";
 import Card from "./Components/Card";
+import bgBlue from "../../public/Assets/Home/bgBlue.jpg";
+import bgContact from "../../public/Assets/Home/slider1.jpg";
+import faq from "../../public/Assets/Home/faq.jpg";
+import SchoolView from "./Components/SchoolView";
+import Accordion from "./Components/Faq";
+import Footer from "./Components/Footer";
+import Image from "next/image";
 
 export default function Home() {
   return (
@@ -33,6 +40,96 @@ export default function Home() {
           ))
         }
       </div>
+
+      <div
+        style={{ background: `linear-gradient(#0a364fc6,#0a364fc6),url(${bgContact.src}) center/cover fixed` }}
+        className="bgContactSection md:py-20 py-8 w-full mt-10">
+        <div className="max-w-7xl w-full mx-auto gap-5 flex items-center md:flex-row flex-col px-4 justify-center">
+          <div className="md:max-w-[50%] w-full">
+            <h2 className="font-bold text-[1.7rem] text-white md:text-[2.5rem] md:pt-16 pt-12 ">CONTACT US</h2>
+            <div className="w-10 mt-2 h-1 rounded-full bg-zinc-300 "></div>
+            <p className="mt-6 text-zinc-300 text-center">
+              We're to assist you! Whether you have questions about our programs, need guidance, or require technical support, feel free to reach out.
+              <br /><br />
+              Our team is dedicated to providing the help you need for a smooth and enriching experience. Simply fill out the form, and we'll get back to you as soon as possible.
+              <br /><br />
+              <i>Ofada Technical School</i></p>
+          </div>
+          <div className="md:max-w-[50%] w-full">
+            <form className="bg-white rounded-lg p-5 grid md:grid-cols-2 grid-cols-1 gap-5 max-w-[30rem] w-full mx-auto md:py-8">
+              <input type="text" placeholder="Name, Lastname *" name="name" required className="py-3 border-b border-zinc-300 focus:border-zinc-400" />
+              <input type="number" placeholder="Mobile Phone *" required name="phone" className="py-3 border-b border-zinc-300 focus:border-zinc-400" />
+              <input type="email" placeholder="Email Address *" required name="email" className="py-3 border-b border-zinc-300 focus:border-zinc-400" />
+              <select name="course" id="course" className="py-3 text-zinc-500 border-b border-zinc-300 focus:border-zinc-400">
+                <option value="Select Course " className="">Choose Your Service</option>
+                <option value="Select Course " className="">Technical Support</option>
+                <option value="Select Course " className="">Course on offer</option>
+                <option value="Select Course " className="">Fee enquiries</option>
+                <option value="Select Course " className="">others</option>
+              </select>
+              <textarea name="message" id="message" rows="3" placeholder="Message" className="py-3 border-b md:col-span-2 resize-none border-zinc-300 focus:border-zinc-400"></textarea>
+              <button className="hover:bg-black  font-medium px-7 md:col-span-2 cursor-pointer transition-all duration-500 bg-[#41c4ff] text-white  py-4 rounded-lg uppercase">SUBMIT</button>
+            </form>
+          </div>
+        </div>
+      </div>
+
+
+
+
+      <h2 className="font-bold text-[1.7rem] px-4 md:text-[2.5rem] md:pt-16 pt-12 text-center">SCHOOL VIEW</h2>
+      <div className="w-10 mt-3 h-1 rounded-full bg-zinc-400 mx-auto"></div>
+      <p className="text-zinc-700 max-w-6xl p-5 text-center mx-auto ">With different departments, Ofada Technical School focuses on providing specialized education and practical skills in various trades and vocations.
+        They include Engineering,Information Technology, Fashion Designing, Chemical and Allied etc.</p>
+      <div className="md:pt-10 max-w-7xl w-full mx-auto p-4 grid md:grid-cols-3 sm:grid-cols-2 grid-cols-1 gap-3 md:gap-5">
+        <SchoolView />
+      </div>
+      <div className="flex justify-center my-7">
+        <button className="hover:bg-black  font-medium px-10 py-4 cursor-pointer transition-all duration-500 bg-[#41c4ff] text-white   rounded-lg uppercase">VIEW MORE</button>
+      </div>
+
+
+
+
+      <div
+        style={{ background: `linear-gradient(#0a364fc6,#0a364fc6),url(${faq.src}) center/cover fixed` }}
+        className="bgContactSection md:pt-20 relative pt-8 w-full mt-10">
+          <div className="h-40 w-full absolute bg-white bottom-0 left-0"></div>
+        <h2 className="font-bold text-[1.7rem] text-white md:text-[2.5rem] text-center md:pt-16 pt-12 ">Faq’s</h2>
+        <div className="w-10 mt-2 h-1 rounded-full bg-zinc-300 mx-auto"></div>
+        <p className="mt-6 text-zinc-300 text-center">
+          Here are the common FAQs</p>
+        <div className="max-w-7xl w-full mx-auto md:mt-14 mt-8 grid md:grid-cols-2 z-10 relative grid-cols-1 px-4 justify-center">
+          <div className="">
+            <Image src={faq} alt="img" className="w-full " />
+          </div>
+          <div className=" bg-white h-full flex items-center justify-center">
+            <Accordion />
+          </div>
+        </div>
+      </div>
+
+
+
+
+
+
+      <h2 className="font-bold text-[1.7rem] px-4 md:text-[2.5rem] md:pt-20 pt-12 text-center">Why choose us?</h2>
+      <div className="w-10 mt-3 h-1 rounded-full bg-zinc-400 mx-auto"></div>
+      <div className="md:pt-16 max-w-7xl w-full mx-auto p-4 pt-8 grid md:grid-cols-3 sm:grid-cols-2 grid-cols-1 gap-8 md:gap-14 ">
+        {
+          wyChooseUs.map((data, index) => (
+            <Card key={index} img={data.img} heading={data.heading} para={data.para} />
+          ))
+        }
+      </div>
+
+      <div style={{ background: `linear-gradient(rgba(255, 255, 255, 0.3),rgba(255, 255, 255, 0.3)),url(${bgBlue.src}) center/cover` }} className=" bg-no-repeat w-full md:mt-16 mt-8">
+        <h2 className="text-center font-bold md:text-5xl text-3xl px-4 !leading-tight md:py-16 py-10">Technical Education in 2024</h2>
+      </div>
+
+      <Footer />
+
     </div>
   );
 }
